d9d63d70350e82f5ee421c0825670adf8d562a2c
Herpiko Dwi Aguno <herpiko@gmail.com>
2017-02-05 22:43:10 +0700

    Add optional port for init script.

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,10 @@
 HOST=${HOST:-localhost}
+PORT=${HOST:-27017}
 DB=${DB:-penda}
 set +e
 echo "Setting up initial data with $DB on $HOST"
 mongo $DB --eval 'db.dropDatabase();'
-mongoimport -h $HOST -d $DB -c users --upsert < scripts/users.json
-mongoimport -h $HOST -d $DB -c profiles --upsert < scripts/profiles.json
-mongoimport -h $HOST -d $DB -c categories --upsert < scripts/categories.json
+mongoimport -h $HOST --port $PORT -d $DB -c users --upsert < scripts/users.json
+mongoimport -h $HOST --port $PORT -d $DB -c profiles --upsert < scripts/profiles.json
+mongoimport -h $HOST --port $PORT -d $DB -c categories --upsert < scripts/categories.json
 set -e
